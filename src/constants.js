1c23138f1935de18a3bb7de38cdd32e59d736c3c
Student9227 <153178343+PHCASCStudent@users.noreply.github.com>
2025-06-02 13:48:53 +0000

    Added new objects in map.json and finished the project

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,20 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  pc: `This is my PC. I do game, web and software development. I like JS and Python.
+    Here is my <a href="https://github.com/Student9226" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. It covered testing, scripting, query and programming languages, project management, web development, databases and so much more.`,
+  "sofa-table": `This is my sofa. I like to sit here and watch tutorials and knowlegeable videos.`,
+  tv: `That's my TV.`,
+  bed: `This is my bed. Sleep is important for productivity, so I try to get enough rest.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/Student9226/My-files/blob/main/Sagar%20Virendra%20Kulhare%20Resume.docx" target="_blank">Check it out?</a>
+  Contact me at <a href="mailto:sagarkulhare755@gmail.com">sagarkulhare755@gmail.com</a> if you have any interesting job or internship opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  Text is rendered with HTML/CSS.`,
+  library: `There are a lot of programming books on my shelves.`,
+  exit: `Currently in progress...`,
+  drawer: `This is my drawer. It contains some of my personal belongings.`,
+  dustbin: `This is my dustbin. I try to keep it clean and throw away unnecessary things.`,
+  pot: `This is my plant pot. I like to keep plants around as they improve the atmosphere.`,
+  secret: `This is my secret stash. It's a hidden place where I keep my personal notes and ideas.`,
 };
